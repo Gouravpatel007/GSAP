0153c8d9df47fb70c58d332389fcd049500bb11f
Gourav Patel <kp180284@gmail.com>
2024-11-20 12:10:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline();
+
+tl.from("#nav h3",{
+    y:-50,
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    stagger:0.4
+})
+tl.from("#nav-part2 h4",{
+    y:-50,
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    stagger:0.4 
+})
+
+tl.from("#animation h1",{
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+
+tl.from("img",{
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:0.5,
+    stagger:0.5
+})
